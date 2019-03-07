648a95ef1cfd146a10ab838732a617eef33d65b4
visualstudio <visualstudio@122-PC23>
2019-03-07 11:21:36 -0500

    total

--- Hotel room/MainActivity.cs
+++ Hotel room/MainActivity.cs
@@ -16,38 +16,49 @@ namespace Hotel_room
         EditText etTotalcash, etnight;
         RadioButton rdbsui, rdbdou, rdbsin;
         CheckBox cbtran, cbsp, cbtg;
+      //  Button btOkay;
         double total = 0;
+        
         protected override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
             SetContentView(Resource.Layout.activity_main);
+            /*
+                 //       etTotalcash = (EditText)FindViewById(Resource.Id.etTotal);
+                        etnight = (EditText)FindViewById(Resource.Id.etNights);
+                        rdbsui = (RadioButton)FindViewById(Resource.Id.rdbSuite);
+                        rdbdou = (RadioButton)FindViewById(Resource.Id.rdbDouble);
+                        rdbsin = (RadioButton)FindViewById(Resource.Id.rdbSingle);
+                        cbtran = (CheckBox)FindViewById(Resource.Id.cbTrans);
+                        cbsp = (CheckBox)FindViewById(Resource.Id.cbSpa);
+                        cbtg = (CheckBox)FindViewById(Resource.Id.cbTourist);
+                  //      btOkay = (Button)FindViewById(Resource.Id.btnOkay);
 
-            etTotalcash = (EditText)FindViewById(Resource.Id.etTotal);
-            etnight = (EditText)FindViewById(Resource.Id.etNights);
-            rdbsui = (RadioButton)FindViewById(Resource.Id.rdbSuite);
-            rdbdou = (RadioButton)FindViewById(Resource.Id.rdbDouble);
-            rdbsin = (RadioButton)FindViewById(Resource.Id.rdbSingle);
-            cbtran = (CheckBox)FindViewById(Resource.Id.cbTransp);
-            cbsp = (CheckBox)FindViewById(Resource.Id.cbSpa);
-            cbtg = (CheckBox)FindViewById(Resource.Id.cbTouristGuide);
+            //   if(rdbsui.Checked)
+            //   {
+            //       total = 1400 * etnight.ToString();
 
-         //   if(rdbsui.Checked)
-         //   {
-         //       total = 1400 * etnight.ToString();
+            //   }
 
-         //   }
+        /*    btOkay.Click += delegate
+            {
+                if (cbtran.Checked)
+                    total = 1000;
+                if (cbsp.Checked)
+                    total = 700;
+                if (cbtg.Checked)
+                    total = 2000;
 
-            if (cbtran.Checked)
-                total = 1000;
-            if (cbsp.Checked)
-                total = 700;
-            if (cbtg.Checked)
-                total = 2000;
+                etTotalcash.Text = total.ToString();
+            };
+                    
+          */  
+          
 
-            etTotalcash.Text = total.ToString();
+
+            
 
         }
-
         public override bool OnCreateOptionsMenu(IMenu menu)
         {
             MenuInflater.Inflate(Resource.Menu.menu_main, menu);
